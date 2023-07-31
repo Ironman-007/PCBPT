43912957dd11fc4e17dcc1bbd7c38e286048f087
Fangzheng Liu <fzliu@mit.edu>
2023-07-30 23:01:29 -0400

    udated by FZ

--- PCBPT_V2/firmware/Arduino/Coral_stepper_module/src/cmd.cpp
+++ PCBPT_V2/firmware/Arduino/Coral_stepper_module/src/cmd.cpp
@@ -1,18 +1,43 @@
+#include <PacketSerial.h>
+
 #include "cmd.h"
 
-volatile uint8_t cmd = 0x00;
+#define RECV_DATA_LEN  5
+#define COE_D_TO_STEPS 40 // needs to be calibrated
 
+volatile uint8_t cmd = 0x00;
 volatile bool got_data;
+volatile uint8_t data_recv_cnt = 0;
+
+float    distance2move = 0.0;
+uint64_t move_steps    = 0;
+
+uint8_t encoded_cmd[RECV_DATA_LEN] = {0x00};
+uint8_t decoded_cmd[RECV_DATA_LEN] = {0x00};
+
+COBS cobs;
 
 void comm_init(void) {
   Wire1.begin(50);
 }
 
 void handle_data(void) {
-  if (Wire1.available()) {
+  if (Wire1.available() > 0) {
     cmd = Wire1.read();
-    Serial.println(cmd);
-    // Coral_stepper.run((uint64_t)cmd);
+    if (cmd == 0) {
+      data_recv_cnt = 0;
+      cobs.decode(encoded_cmd, RECV_DATA_LEN, decoded_cmd);
+      memcpy(&distance2move, decoded_cmd, RECV_DATA_LEN-1);
+      Serial.print("distance2move = "); Serial.println(distance2move);
+      // TODO:
+      //   - Record current position and calculate how much distance to move
+      move_steps = distance2move * COE_D_TO_STEPS;
+      Coral_stepper.run(move_steps)
+    }
+    else {
+      encoded_cmd[data_recv_cnt] = cmd;
+      data_recv_cnt ++;
+    }
   }
 }
 
--- PCBPT_V2/firmware/Host_ESP32S2/code.py
+++ PCBPT_V2/firmware/Host_ESP32S2/code.py
@@ -3,37 +3,31 @@ import busio
 import time
 import supervisor
 import digitalio
+import struct
+
+Coral_addr_1 = '0x32'
+Coral_addr_2 = '0x33'
+Coral_addr_3 = '0x34'
+Coral_addr_4 = '0x35'
+Coral_addr_5 = '0x36'
+Coral_addr_6 = '0x37'
+
+IND1 = digitalio.DigitalInOut(board.IO21)
+IND2 = digitalio.DigitalInOut(board.IO35)
+IND3 = digitalio.DigitalInOut(board.IO33)
+IND4 = digitalio.DigitalInOut(board.IO18)
+IND5 = digitalio.DigitalInOut(board.IO17)
+IND6 = digitalio.DigitalInOut(board.IO16)
+led  = digitalio.DigitalInOut(board.LED)
 
 # i2c = busio.I2C(board.SCL, board.SDA)
-
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
-
-print("PCBPT connected!")
 
 PARAMETER_CNT = 2
 
 cmd_split_dict = {}
 movements      = []
 
-addr_A = 0x40
-addr_B = 0x41
-addr_X = 0x42
-addr_Y = 0x43
-addr_M = 0x44
-addr_N = 0x45
-
-steps2move_A   = 0
-steps2move_B   = 0
-steps2move_X   = 0
-steps2move_Y   = 0
-steps2move_M   = 0
-steps2move_N   = 0
-
-comm_A_exist   = False
-comm_B_exist   = False
-comm_X_exist   = False
-comm_Y_exist   = False
+START_BYTE = 'S'
 
 command_correct = True
 
@@ -52,31 +46,121 @@ def flash_led(t):
     time.sleep(t)
     led.value = False
 
+def Pins_init():
+    IND1.direction = digitalio.Direction.OUTPUT
+    IND2.direction = digitalio.Direction.OUTPUT
+    IND3.direction = digitalio.Direction.OUTPUT
+    IND4.direction = digitalio.Direction.OUTPUT
+    IND5.direction = digitalio.Direction.OUTPUT
+    IND6.direction = digitalio.Direction.OUTPUT
+    led.direction  = digitalio.Direction.OUTPUT
+    
+    IND1.value = False
+    IND2.value = False
+    IND3.value = False
+    IND4.value = False
+    IND5.value = False
+    IND6.value = False
+    led.value  = False
+    
+    return True
+
+def scan_Coral_modules():
+    global i2c
+    i2c = busio.I2C(board.IO39, board.IO37, frequency = 100_000)
+
+    while not i2c.try_lock():
+        pass
+
+    Coral_I2C_addr = [hex(x) for x in i2c.scan()]
+
+    if Coral_addr_1 in Coral_I2C_addr:
+        IND1.value = True
+        print('Found Coral #1')
+    if Coral_addr_2 in Coral_I2C_addr:
+        IND2.value = True
+        print('Found Coral #2')
+    if Coral_addr_3 in Coral_I2C_addr:
+        IND3.value = True
+        print('Found Coral #3')
+    if Coral_addr_4 in Coral_I2C_addr:
+        IND4.value = True
+        print('Found Coral #4')
+    if Coral_addr_5 in Coral_I2C_addr:
+        IND5.value = True
+        print('Found Coral #5')
+    if Coral_addr_6 in Coral_I2C_addr:
+        IND6.value = True
+        print('Found Coral #6')
+
 def check_if_number(str_input):
     for i in range(len(str_input)):
-        if (str_input[i] >= '0' and str_input[i] <= '9' or str_input[i] == '.'):
+        if (str_input[i] >= '0' and str_input[i] <= '9' or str_input[i] == '.' or str_input[i] == '-'):
             continue
         else:
             return False
 
     return True
 
-def calculate_steps(distance):
-    print(distance)
+def cobs_encoding(buffer, size, encodedBuffer):
+    read_index  = 0
+    write_index = 1
+    code_index  = 0
+    code        = 1
 
-    return True
+    while (read_index < size):
+        if (buffer[read_index] == 0):
+            encodedBuffer[code_index] = code
+            code = 1
+            code_index = write_index
+            write_index += 1
+            read_index += 1
 
-def send_cmd(address, data):
-    print(address)
-    print(data[0])
-    print(data[1])
-    print(data[2])
-    print(data[3])
+        else:
+            encodedBuffer[write_index] = buffer[read_index]
+            write_index += 1
+            read_index += 1
+            code += 1
+
+            if (code == 0xFF):
+                encodedBuffer[code_index] = code
+                code = 1
+                code_index = write_index
+                write_index += 1
+
+    encodedBuffer[code_index] = code
+
+    return write_index
+
+def send_cmd(addr, pos_bytes):
+    print("addr: ", addr)
+    print("pos_bytes: ", pos_bytes)
+
+    Bytes = bytes(pos_bytes)
+
+    COBS_bytes = [0] * 5
+    cobs_encoding(Bytes, 4, COBS_bytes)
+    print(COBS_bytes)
+
+    addr_int = int(addr)
+
+    i2c.writeto(addr_int, bytes(COBS_bytes))
+    i2c.writeto(addr_int, bytes([0]))
 
     return True
 
 Probe1 = Probe(0.0, 0.0)
 Probe2 = Probe(0.0, 0.0)
+
+# initialize all IOs to be used
+Pins_init()
+
+if supervisor.runtime.serial_connected:
+    flash_led(0.05)
+    print("PCBPT connected!")
+
+# Sca I2C bus and find all Coral modules
+scan_Coral_modules()
 
 while True:
     if supervisor.runtime.serial_bytes_available:
@@ -143,36 +227,43 @@ while True:
 
         if "A" in cmd_split_dict:
             comm_A_exist =  True
-            A_move = cmd_split_dict["A"]
+            A_pos = cmd_split_dict["A"]
         if "B" in cmd_split_dict:
             comm_B_exist =  True
-            B_move = cmd_split_dict["B"]
+            B_pos = cmd_split_dict["B"]
 
         if "X" in cmd_split_dict:
             comm_X_exist =  True
-            X_move = cmd_split_dict["X"]
+            X_pos = cmd_split_dict["X"]
         if "Y" in cmd_split_dict:
             comm_Y_exist =  True
-            Y_move = cmd_split_dict["Y"]
+            Y_pos = cmd_split_dict["Y"]
 
         if (comm_A_exist == True and comm_B_exist == True):
-            print("Probe #1 move to ", (A_move, B_move))
-            Probe1.Target_X = A_move
-            # if (Probe1.Target_X == Probe1.Current_X):
-            #   pass
-            # else:
-            #   steps = calculate_steps(Probe1.Target_X - Probe1.Current_X)
-            #   send_cmd(addr_A, steps)
-            #   Probe1.Current_X = A_move
-            Probe1.Target_Y = B_move
+            print("Probe #1 move to ", (A_pos, B_pos))
+            Probe1.Target_X = float(A_pos)
+            print(Probe1.Target_X)
+
+            if (Probe1.Target_X == Probe1.Current_X):
+                print("Probe #1 X not changing")
+                pass
+            else:
+              # Translate Probe1.Target_X to bytes array pos_bytes
+              pos_bytes = bytearray(struct.pack("f", Probe1.Target_X))  
+              # send it to Probe1
+              send_cmd(Coral_addr_1, pos_bytes)
+              Probe1.Current_X = Probe1.Target_X
+
+            # Probe1.Target_Y = B_pos
             # if (Probe1.Target_Y == Probe1.Current_Y):
-            #   pass
+            #     print("Probe #1 Y not changing")
+            #     pass
             # else:
             #   steps = calculate_steps(Probe1.Target_Y - Probe1.Current_Y)
             #   send_cmd(addr_B, steps)
             #   Probe1.Current_Y = B_move
         if (comm_X_exist == True and comm_Y_exist == True):
-            print("Probe #2 move to ", (X_move, Y_move))
+            print("Probe #2 move to ", (X_pos, Y_pos))
             Probe2.Target_Y = X_move
             # if (Probe2.Target_X == Probe2.Current_X):
             #   pass
@@ -187,7 +278,4 @@ while True:
             #   steps = calculate_steps(Probe2.Target_Y - Probe2.Current_Y)
             #   send_cmd(addr_Y, steps)
             #   Probe2.Current_Y = Y_move
-
-
-
 
