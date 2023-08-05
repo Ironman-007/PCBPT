28efcf42bdf2fe36445819ccc4a56b290b0bf3a2
Fangzheng Liu <fzliu@mit.edu>
2023-08-04 22:59:33 -0400

    udated by FZ

--- PCBPT_V2/firmware/Arduino/Coral_stepper_module_Seperate_Config/src/cmd.cpp
+++ PCBPT_V2/firmware/Arduino/Coral_stepper_module_Seperate_Config/src/cmd.cpp
@@ -9,6 +9,8 @@ volatile uint8_t cmd           = 0x00;
 volatile uint8_t data_recv_cnt = 0;
 volatile bool    got_data      = false;
 
+Coral_state coral_state;
+
 float move2pos               = 0.0;
 
 volatile float target_pos    = 0.0;
@@ -20,14 +22,62 @@ uint8_t decoded_cmd[RECV_DATA_LEN] = {0x00};
 
 COBS cobs;
 
+void set_coral_state(uint8_t done, float current, float target) {
+  coral_state.motion_done = done;
+  coral_state.currentpos  = current;
+  coral_state.targetpos   = target;
+}
+
+void set_coral_state_done(uint8_t done) {
+  if (SERIAL_DEBUG) {
+    Serial.print("set_coral_state_done");
+    Serial.println(done);
+  }
+
+  coral_state.motion_done = done;
+}
+
+void set_coral_state_currentpos(float current) {
+  if (SERIAL_DEBUG) {
+    Serial.print("set_coral_state_currentpos");
+    Serial.println(current);
+  }
+
+  coral_state.currentpos  = current;
+}
+
+void set_coral_state_targetpos(float target) {
+  if (SERIAL_DEBUG) {
+    Serial.print("set_coral_state_targetpos");
+    Serial.println(target);
+  }
+
+  coral_state.targetpos   = target;
+}
+
+void report_state(void) {
+  if (SERIAL_DEBUG) {
+    Serial.println("Report state:");
+    Serial.println(coral_state.motion_done);
+    Serial.println(coral_state.currentpos);
+    Serial.println(coral_state.targetpos);
+  }
+
+  Wire1.write((byte *)&coral_state, sizeof(coral_state));
+}
+
 void comm_init(void) {
   Wire1.begin(Coral.I2C_ADDR);
+  Wire1.onRequest(report_state);
+
+  set_coral_state(0, 0.0, 0.0);
 }
 
 void handle_data(void) {
   if (!stepper_homed) {
     Coral_stepper.home();
     Coral_stepper.set_current_pos(Coral.HOME_POS);
+    set_coral_state_currentpos(Coral.HOME_POS);
     stepper_homed = true;
   }
 
@@ -40,13 +90,13 @@ void handle_data(void) {
         memcpy(&move2pos, decoded_cmd, RECV_DATA_LEN-1);
         target_pos = move2pos;
 
+        set_coral_state_targetpos(target_pos);
+
         if (SERIAL_DEBUG) {
           Serial.print("move2pos = ");
           Serial.println(target_pos);
         }
-
         got_data = true;
-
       }
       else {
         encoded_cmd[data_recv_cnt] = cmd;
@@ -64,29 +114,32 @@ void control_motion(void) {
       if (target_pos >= Coral.HOME_POS) {
         Coral_stepper.home();
         Coral_stepper.set_current_pos(Coral.HOME_POS);
+        set_coral_state_currentpos(Coral.HOME_POS);
       }
       else {
-          distance2move = target_pos - Coral_stepper.current_pos;
+        distance2move = target_pos - Coral_stepper.current_pos;
 
-          if (SERIAL_DEBUG) {
-            Serial.print("distance2move = ");
-            Serial.println(round(distance2move));
-          }
-
-          Coral_stepper.run(distance2move);
-
-          if (SERIAL_DEBUG) {
-            Serial.print("move_steps = ");
-            Serial.println(round(move_steps));
-          }
-
-          Coral_stepper.set_current_pos(target_pos);
+        if (SERIAL_DEBUG) {
+          Serial.print("distance2move = ");
+          Serial.println(round(distance2move));
         }
+
+        Coral_stepper.run(distance2move);
+
+        if (SERIAL_DEBUG) {
+          Serial.print("move_steps = ");
+          Serial.println(round(move_steps));
+        }
+
+        Coral_stepper.set_current_pos(target_pos);
+        set_coral_state_currentpos(target_pos);
+      }
     }
     else {
       if (target_pos <= Coral.HOME_POS) {
         Coral_stepper.home();
         Coral_stepper.set_current_pos(Coral.HOME_POS);
+        set_coral_state_currentpos(Coral.HOME_POS);
       }
       else {
           distance2move = target_pos - Coral_stepper.current_pos;
@@ -104,6 +157,7 @@ void control_motion(void) {
           }
 
           Coral_stepper.set_current_pos(target_pos);
+          set_coral_state_currentpos(target_pos);
         }
       }
     }
--- PCBPT_V2/firmware/Arduino/Coral_stepper_module_Seperate_Config/src/cmd.h
+++ PCBPT_V2/firmware/Arduino/Coral_stepper_module_Seperate_Config/src/cmd.h
@@ -4,6 +4,9 @@
 
 #include "coral_stepper.h"
 
+#define MOTION_DONE 1
+#define MOVING      0
+
 extern volatile bool got_data;
 
 extern volatile uint8_t cmd;
@@ -14,3 +17,18 @@ extern void comm_init(void);
 extern void handle_data(void);
 extern void control_motion(void);
 
+struct Coral_state {
+    volatile uint8_t motion_done;
+
+    volatile float   currentpos;
+    volatile float   targetpos;
+};
+
+extern Coral_state coral_state;
+
+extern void set_coral_state(uint8_t done, float current, float target);
+extern void set_coral_state_done(uint8_t done);
+extern void set_coral_state_currentpos(float current);
+extern void set_coral_state_targetpos(float target);
+
+extern void report_state(void);
