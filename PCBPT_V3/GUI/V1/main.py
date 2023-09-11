43ad35cc6d46849253534a86d81ec8e1a071c0d4
Fangzheng Liu <fzliu@mit.edu>
2023-09-11 16:07:35 -0400

    udated by FZ

--- PCBPT_V3/GUI/V1/main.py
+++ PCBPT_V3/GUI/V1/main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+from kiutils.board import Board
+from kiutils.libraries import LibTable
+from kiutils.schematic import Schematic
+from kiutils.footprint import Footprint
+from kiutils.symbol import SymbolLib
+from kiutils.wks import WorkSheet
+from kiutils.dru import DesignRules
+from kiutils.items.fpitems import FpText
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from os import path
 
-import
-def print_hi(name):
+def get_net(board_in):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    for network in board_in.nets:
+        print(network)
 
+def get_components(sch_in):
+    # Use a breakpoint in the code line below to debug your script.
+    for symbol in sch_in.schematicSymbols:
+        print(symbol)
+
+def get_footprint(board_in):
+    # Use a breakpoint in the code line below to debug your script.
+    for footprint in board_in.footprints:
+        pad = footprint.pads
+        print(len(pad))
+        print(pad[0].position)
+        # if (fptext[0].type == 'reference'):
+        #     print(fptext[0].text)
+
+board_path = "C:\DATA\Documents\Lab\github\PCBPT\PCBPT_V3\PCB\Board_To_Be_Tested_V2\Board_To_Be_Tested.kicad_pcb"
+board_sch_path = "C:\DATA\Documents\Lab\github\PCBPT\PCBPT_V3\PCB\Board_To_Be_Tested_V2\Board_To_Be_Tested.kicad_sch"
+
+board = Board().from_file(board_path)
+sch   = Schematic().from_file(board_sch_path)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # get_net(board)
+    # get_components(sch)
+    get_footprint(board)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
