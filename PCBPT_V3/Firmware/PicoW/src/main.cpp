498cbf9eddbbe9466f5f95376f8d9c98c0a3fb09
Fangzheng Liu <fzliu@mit.edu>
2023-09-13 16:55:58 -0400

    udated by FZ

--- PCBPT_V3/Firmware/PicoW/src/main.cpp
+++ PCBPT_V3/Firmware/PicoW/src/main.cpp
@@ -2,11 +2,18 @@
 // Created by Fangzheng on 9/5/2023.
 //
 #include "Arduino.h"
+#include "pico/stdlib.h"
+
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 void setup() {
-  ;
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
 }
 
 void loop() {
-  ;
+  gpio_put(LED_PIN, 1);
+  sleep_ms(500);
+  gpio_put(LED_PIN, 0);
+  sleep_ms(500);
 };
