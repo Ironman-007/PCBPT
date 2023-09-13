154192e560610f704ed33c9228e97a66fbfe56e1
Fangzheng Liu <fzliu@mit.edu>
2023-09-13 17:32:23 -0400

    udated by FZ

---
+++ PCBPT_V3/Firmware/PicoW/src/comm.cpp
@@ -0,0 +1,10 @@
+//
+// Created by l1534 on 9/13/2023.
+//
+
+#include "Arduino.h"
+#include "comm.h"
+
+void comm_init(void) {
+  Serial.begin(115200);
+}
---
+++ PCBPT_V3/Firmware/PicoW/src/comm.h
@@ -0,0 +1,21 @@
+//
+// Created by l1534 on 9/13/2023.
+//
+
+#ifndef __COMM_H
+#define __COMM_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+extern void comm_init(void);
+
+// your normal definitions here
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //PICOW_COMM_H
