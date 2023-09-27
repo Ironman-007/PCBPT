b14fc7c6dc2f99e7cadda592de9ba21c5362aed7
Fangzheng Liu <fzliu@mit.edu>
2023-09-27 15:22:37 -0400

    udated by FZ

---
+++ PCBPT_V3/Machine_code/PicoW/src/stepper_FZ.h
@@ -0,0 +1,17 @@
+//
+// Created by l1534 on 9/13/2023.
+//
+
+#ifndef __STEPPER_FZ_H
+#define __STEPPER_FZ_H
+
+#include "Arduino.h"
+
+extern float motion_A;
+extern float motion_B;
+extern float motion_X;
+extern float motion_Y;
+
+extern void stepper_init(void);
+
+#endif //PICOW_STEPPER_FZ_H
