14ecc97add2df48317fe627889a14f7b62b5eadb
Fangzheng Liu <fzliu@mit.edu>
2023-10-03 13:18:31 -0400

    udated by FZ

--- PCBPT_V3/Machine_code/PicoW/src/stepper_FZ.h
+++ PCBPT_V3/Machine_code/PicoW/src/stepper_FZ.h
@@ -7,11 +7,15 @@
 
 #include "Arduino.h"
 
+#define stepper_res_1 0.1 // how much the linear motion is in one step
+
 extern float motion_A;
 extern float motion_B;
 extern float motion_X;
 extern float motion_Y;
 
 extern void stepper_init(void);
+
+extern void start_motion(void);
 
 #endif //PICOW_STEPPER_FZ_H
